6b2ad8106344439903246ebd22a82d3544f32f25
Christopher McCulloh <cmcculloh@exacttarget.com>
2015-09-04 15:40:08 -0400

    (master) moved the test app into tests

--- tests/tests.js
+++ tests/tests.js
@@ -5,19 +5,19 @@ define(function (require) {
 
 	var selected = '[selected=selected]';
 	var tree = 'ul'
-	console.log('first path', getListItemPath(tree, $(selected).first()).join('/'));
+	$('#paths').append('<p>').append('first path: ', getListItemPath(tree, $(selected).first()).join('/'));
 
 	getListItemPaths(tree, selected).forEach(function (path) {
-		console.log('all paths separate', path.join('>'));
+		$('#paths').append('<p>').append('all paths separate: ', path.join('>'));
 	});
 
 	var selectedPaths = getListItemPaths(tree, selected);
 	var selectedPathsString = $.map(selectedPaths, function(path){
 		return path.join('/');
 	}).join(', ');
-	console.log('all paths together', selectedPathsString);
+	$('#paths').append('<p>').append('all paths together: ', selectedPathsString);
 
-	console.log('simplified first path', getListItemPath(tree, $(selected).first(), undefined, '/'));
-	console.log('simplified all paths', getListItemPaths(tree, selected, undefined, '/', ', '));
+	$('#paths').append('<p>').append('simplified first path: ', getListItemPath(tree, $(selected).first(), undefined, '/'));
+	$('#paths').append('<p>').append('simplified all paths: ', getListItemPaths(tree, selected, undefined, '/', ', '));
 
 });
